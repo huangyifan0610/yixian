241522388c76ed04aa6b5746f24dd4b64b5b5917
Huang Yifan <yifan0610@foxmail.com>
2026-03-20 06:19:18 +0800

    Add MOD entry point

---
+++ src/Main.cs
@@ -0,0 +1,42 @@
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Logging;
+using MegaCrit.Sts2.Core.Modding;
+
+namespace Yixian;
+
+/// <summary>
+/// The Mod initializer.
+/// </summary>
+[ModInitializer(nameof(Run))]
+public sealed class Main
+{
+    /// <summary>
+    /// The Mod author.
+    /// </summary>
+    public static readonly string AUTHOR = "huangyifan0610";
+
+    /// <summary>
+    /// A unique ID indentifing the Mod.
+    /// </summary>
+    public static readonly string ID = "yixian";
+
+    /// <summary>
+    /// GitHub repository URL.
+    /// </summary>
+    public static readonly string URL = "https://github.com/" + AUTHOR + "/" + ID;
+
+    /// <summary>
+    /// Mod-level Logger.
+    /// </summary>
+    public static Logger Logger { get; } = new(ID, LogType.Generic);
+
+    /// <summary>
+    /// The initializer method.
+    /// </summary>
+    public static void Run()
+    {
+        /// Harmony Patches.
+        var harmony = new Harmony(ID);
+        harmony.PatchAll();
+    }
+}
